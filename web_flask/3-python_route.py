d316f7e25c172e46f12c8fec2f25b1f0036594df
Nehemiembg <nehemie.mbg@gmail.com>
2022-11-29 01:39:25 -0800

    Displays "Python + text"

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Script that starts a Flask a web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello_hbnb():
+    '''Display "Hello HBNB" '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    '''Display "HBNB" '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    '''Display "C following by the text'''
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    '''Listen to this port'''
+    app.run(host='0.0.0.0', port=5000)
